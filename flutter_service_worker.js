2386515b80e6f95cc99cccf2b435c1b492e43c5c
0xvls <102376709+0xvls@users.noreply.github.com>
2026-02-23 21:35:50 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "59f88645b861ff989d9ccef88ebf9868",
+const RESOURCES = {"flutter_bootstrap.js": "18f507f93c2f8371514aea9bdbdbb6e1",
 "version.json": "056a45de1b3e8de8e11d14377726076c",
 "index.html": "a5b756e1f317499ee40fffd249d221d4",
 "/": "a5b756e1f317499ee40fffd249d221d4",
-"main.dart.js": "422e1a740308343a2bac247540706678",
+"main.dart.js": "87ce3e989ddf2e4c503cb4cdc6ceecd2",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
